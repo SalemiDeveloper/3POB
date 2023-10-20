9ee4c99b1d9a46a62187c08ed84f77d5ddb2a0e1
Pedro Salemi <128959821+SalemiDeveloper@users.noreply.github.com>
2023-10-20 08:31:14 -0300

    itemcarrinho.java

---
+++ itemcarrinho.java
@@ -0,0 +1,21 @@
+public class ItemCarrinho 
+{
+    private int idProduto;
+    private int quantidade;
+
+    public ItemCarrinho(int idProduto, int quantidade) 
+    {
+        this.idProduto = idProduto;
+        this.quantidade = quantidade;
+    }
+
+    public int getIdProduto() 
+    {
+        return idProduto;
+    }
+
+    public int getQuantidade() 
+    {
+        return quantidade;
+    }
+}
